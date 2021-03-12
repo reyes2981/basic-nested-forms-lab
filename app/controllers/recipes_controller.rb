2084de31628106506229ca7a732d4f0d72f3260c
reyes2981 <noe.reyes92@yahoo.com>
2021-03-12 08:22:03 -0600

    commit tree 1c10daa5e0ef94b9fe0d462d0e45153730a4446b

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,22 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+      if @recipe.save
+        redirect_to recipe_path(@recipe), notice: "Your recipe was created"
+      end
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [ :name, :quantity ] )
+  end
+
+
 end
